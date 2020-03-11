f96e37d8deb67df4331cc11dea37a75dff38352b
princelewis <princelewis4christ@yahoo.com>
2020-03-11 13:42:41 +0100

    Configure main application class for converting date types to MySql date types when persisting on Database

--- src/main/java/com/twitter/polls/PollsApplication.java
+++ src/main/java/com/twitter/polls/PollsApplication.java
@@ -2,9 +2,28 @@ package com.twitter.polls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
+//This houses @EnableAutoConfiguration
+//@Configuration
+//@ComponentScan
 @SpringBootApplication
+
+//The main purpose of this is to access the class Jsr310JpaConverter
+//to convert the date classes we have on our entity to MySql
+//date-type when during persistence
+@EntityScan(basePackageClasses = Jsr310JpaConverters.class)
 public class PollsApplication {
+
+    @PostConstruct
+    void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PollsApplication.class, args);
