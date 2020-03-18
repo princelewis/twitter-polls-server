cc4469aad5dd39a60da52c8c11e96173519da6c7
princelewis <princelewis4christ@yahoo.com>
2020-03-18 23:23:18 +0100

    Add more fields to User entity

--- src/main/java/com/twitter/polls/model/User.java
+++ src/main/java/com/twitter/polls/model/User.java
@@ -10,30 +10,73 @@ import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
+
 @Data
 @Table(name = "users")
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
 
     @NotBlank
     @Size(max = 15)
+    @Column(name = "username")
     private String username;
 
     @NotBlank
     @Size(max = 40)
     @Email
     @NaturalId
+    @Column(name = "email")
     private String email;
 
     @NotBlank
     @Size(max = 100)
+    @Column(name = "password")
     private String password;
 
     @OneToMany(fetch = FetchType.LAZY)
     private Set<Role> roles = new HashSet<>();
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
