98c1143c26b9f5a814b847ace8965e9c44c09244
princelewis <princelewis4christ@yahoo.com>
2020-03-23 10:07:51 +0100

    Create an audit class for entity classes that need auditing

---
+++ src/main/java/com/twitter/polls/model/audit/DateAudit.java
@@ -0,0 +1,36 @@
+package com.twitter.polls.model.audit;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.Instant;
+
+@MappedSuperclass // This designates a class whose mapping
+//information is applied to the entities that inherits from it
+@Data
+@Getter(AccessLevel.PROTECTED)
+@Setter(AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class) //This is used to specify
+//listener classes which will listen for the event of the entities through some annotated methods
+public class DateAudit {
+
+//    @CreationTimestamp
+    @CreatedDate
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+//    @UpdateTimestamp
+    @LastModifiedDate
+    private Instant updatedAt;
+}
