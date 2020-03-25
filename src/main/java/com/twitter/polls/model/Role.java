5aced5daa913ca75578aa8b8cbd2c112b01e0a42
princelewis <princelewis4christ@yahoo.com>
2020-03-25 13:05:07 +0100

    Modify Role entity class for bug fixing

--- src/main/java/com/twitter/polls/model/Role.java
+++ src/main/java/com/twitter/polls/model/Role.java
@@ -1,32 +1,40 @@
 package com.twitter.polls.model;
 
 import lombok.Data;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
 @Data
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
-    @Enumerated(value = EnumType.STRING)
+    @Enumerated(EnumType.STRING)
+    @NaturalId
     @Column(length = 60)
-    private RoleName roleName;
+    private RoleName name;
 
-    @ManyToOne
-    private User user;
+    @OneToMany
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> user = new HashSet<>();
 
     public Role(){
 
     }
 
-    public Role(RoleName roleName){
-        this.roleName = roleName;
+    public Role(RoleName name){
+        this.name = name;
     }
 
 }
