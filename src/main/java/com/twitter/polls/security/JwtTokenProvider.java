b15de6709a80cc2d8c72a60149c3432231eed4dd
princelewis <princelewis4christ@yahoo.com>
2020-04-01 15:51:39 +0100

    Create JWT token provider class for generating token

---
+++ src/main/java/com/twitter/polls/security/JwtTokenProvider.java
@@ -0,0 +1,42 @@
+package com.twitter.polls.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private Long jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication){
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+        return Jwts.builder()
+                .setSubject(userPrincipal.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.ES512, jwtSecret)
+                .compact();
+    }
+
+
+}
