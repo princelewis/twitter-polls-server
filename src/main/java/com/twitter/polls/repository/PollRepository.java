b52dae7dcc8e83577ca1b7dce86b574d49dc2227
princelewis <princelewis4christ@yahoo.com>
2020-04-12 23:49:07 +0100

    Create PollRepository interface for setting up methods to manipulate the database

---
+++ src/main/java/com/twitter/polls/repository/PollRepository.java
@@ -0,0 +1,26 @@
+package com.twitter.polls.repository;
+
+import com.twitter.polls.model.Poll;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PollRepository extends JpaRepository<Poll, Long> {
+
+    Optional<Poll> findById(Long pollId);
+
+    Optional<Poll> findByCreatedBy(Long UserId, Pageable pageable);
+
+    long countByCreatedBy(Long userId);
+
+    List<Poll> findByIdIn(List<Long> pollIds);
+
+    List<Poll> findByIdIn(List<Long> pollIds, Sort sort);
+
+
+}
