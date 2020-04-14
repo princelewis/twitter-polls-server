e646a4c747cc489a56d26307f92c0753224d1164
princelewis <princelewis4christ@yahoo.com>
2020-04-15 00:04:58 +0100

    Create PollRequest dto

---
+++ src/main/java/com/twitter/polls/payload/PollRequest.java
@@ -0,0 +1,27 @@
+package com.twitter.polls.payload;
+
+import com.twitter.polls.model.Choice;
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+public class PollRequest {
+
+    @NotBlank
+    @Size(max = 140)
+    private String question;
+
+    @NotNull
+    @Size(min = 2, max = 6)
+    @Valid
+    private List<ChoiceRequest> choices;
+
+    @NotNull
+    @Valid
+    private PollLength pollLength;
+}
