5fee497db89fe61b0c011c768e7706abe05628bd
princelewis <princelewis4christ@yahoo.com>
2020-04-20 22:07:33 +0100

    Addn JPA method for getting all users with Ids in a collection of Ids

--- src/main/java/com/twitter/polls/repository/UserRepository.java
+++ src/main/java/com/twitter/polls/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.twitter.polls.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,8 +13,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     //Check the order hibernate picks the parameters in this method
     Optional<User> findByUsernameOrEmail(String username, String Email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
     Optional<User> findByUsername(String username);
+
     Boolean existsByUsername(String username);
+
     Boolean existsByEmail (String email);
 
 }
